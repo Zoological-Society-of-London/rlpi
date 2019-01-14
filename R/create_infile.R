c9be91b419c5d2d0004e650d4fa3809394da4a20
valemarc <15122115+valemarc@users.noreply.github.com>
2019-01-14 17:23:43 +0000

    Update create_infile.R

--- R/create_infile.R
+++ R/create_infile.R
@@ -6,7 +6,7 @@
 #' @return Returns a data frame with (Binomial, ID, year, popvalue) columns
 #' @export
 #'
-convert_to_rows <- function(in_data, start_col_name="X1950", end_col_name="X2015") {
+convert_to_rows <- function(in_data, start_col_name="X1950", end_col_name="X2017") {
   #Binomial  ID  year  popvalue
   all_data <- data.frame(Binomial = character(0), ID = numeric(0), year = numeric(0), popvalue=numeric(0))
   # In input data, each population is a row, with a series of population sizes
@@ -49,7 +49,7 @@ convert_to_rows <- function(in_data, start_col_name="X1950", end_col_name="X2015
 #' @return Returns the name of the created infile
 #' @export
 #'
-create_infile <- function(pop_data_source, index_vector=TRUE, name="default_infile", start_col_name="X1950", end_col_name="X2015", CUT_OFF_YEAR = 1950) {
+create_infile <- function(pop_data_source, index_vector=TRUE, name="default_infile", start_col_name="X1950", end_col_name="X2017", CUT_OFF_YEAR = 1950) {
   # If no index vector is suppled, it will just use all the pop_data_source data
   pop_data <- pop_data_source[index_vector, ]
   all_data <- convert_to_rows(pop_data, start_col_name, end_col_name)
