6db6d40b04cc7e3d3b336e055bf04c10cefd6f5f
LouiseMcRae <louise.mcrae@ioz.ac.uk>
2022-11-30 15:38:06 +0000

    Update LPIMain.R
    Clarified definition of DATA_LENGTH_MIN

--- R/LPIMain.R
+++ R/LPIMain.R
@@ -31,7 +31,7 @@
 #'
 #' @param MODEL_SELECTION_FLAG Default=0
 #' @param GAM_GLOBAL_FLAG - 1 = process by GAM method, 0 = process by chain method. Default=1
-#' @param DATA_LENGTH_MIN - Minimum data length to include in calculations. Default=2
+#' @param DATA_LENGTH_MIN - Minimum number of data points to include in calculations. Default=2
 #' @param AVG_TIME_BETWEEN_PTS_MAX - Maximum time between datapoint to include. Default=100
 #' @param GLOBAL_GAM_FLAG_SHORT_DATA_FLAG - Set this to 1 GAM model is also to be generated for the short time series else the log linear model will be used. Default=0
 #' @param AUTO_DIAGNOSTIC_FLAG - 1=Automatically determine whether GAM models are good enough, 0=Manually ask for each. Default=1
