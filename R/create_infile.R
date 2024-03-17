b1254fc0a1248e4548db7d1d3a46a82b5bc30b5b
robinfreeman <robin.freeman@ioz.ac.uk>
2024-03-17 11:55:18 +0000

    resolved merge conflict, keeping change of end_col_name to X2022

--- R/create_infile.R
+++ R/create_infile.R
@@ -49,6 +49,7 @@ convert_to_rows <- function(in_data, start_col_name = "X1950", end_col_name = "X
 #' @export
 #'
 create_infile <- function(pop_data_source, index_vector = TRUE, name = "default_infile", start_col_name = "X1950", end_col_name = "X2022", CUT_OFF_YEAR = 1950) {
+
   # If no index vector is suppled, it will just use all the pop_data_source data
   pop_data <- pop_data_source[index_vector, ]
   all_data <- convert_to_rows(pop_data, start_col_name, end_col_name)
